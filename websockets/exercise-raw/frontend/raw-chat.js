b617c5c780a5db8358dead171bbcb90765de0db8
suukii <suki@vibe.us>
2021-10-19 09:58:15 +0800

    raw ws: frontend

--- websockets/exercise-raw/frontend/raw-chat.js
+++ websockets/exercise-raw/frontend/raw-chat.js
@@ -11,14 +11,31 @@ chat.addEventListener("submit", function (e) {
 });
 
 async function postNewMsg(user, text) {
-  // code goes here
+  const data = {
+    user,
+    text,
+  };
+
+  ws.send(JSON.stringify(data));
 }
 
-/*
- *
- * your code goes here
- *
- */
+const ws = new WebSocket("ws://localhost:8080", ["json"]);
+
+ws.addEventListener("open", () => {
+  console.log("connection open");
+  presence.innerText = "🟢";
+});
+
+ws.addEventListener("close", () => {
+  console.log("connection close");
+  presence.innerText = "🔴";
+});
+
+ws.addEventListener("message", (e) => {
+  const data = JSON.parse(e.data);
+  allChat = data.msg;
+  render();
+});
 
 function render() {
   const html = allChat.map(({ user, text }) => template(user, text));
