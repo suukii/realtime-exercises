34f6adcfc4ab2a089d9fe1666aa2b80589b35cc0
suukii <suki@vibe.us>
2021-10-19 10:22:58 +0800

    socket.io: frontend

--- websockets/exercise-socketio/frontend/socketio-chat.js
+++ websockets/exercise-socketio/frontend/socketio-chat.js
@@ -5,11 +5,25 @@ const msgs = document.getElementById("msgs");
 const presence = document.getElementById("presence-indicator");
 let allChat = [];
 
-/*
- *
- * Code goes here
- *
- */
+// socket.io will fallback to long polling if ws is not supported
+// window.WebSocket = null;
+
+const socket = io("ws://localhost:8080");
+
+socket.on("connect", () => {
+  console.log("connected");
+  presence.innerText = "🟢";
+});
+
+socket.on("disconnect", () => {
+  console.log("disconnectd");
+  presence.innerText = "🔴";
+});
+
+socket.on('msg:get', data => {
+  allChat = data.msg;
+  render()
+})
 
 chat.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -18,11 +32,12 @@ chat.addEventListener("submit", function (e) {
 });
 
 async function postNewMsg(user, text) {
-  /*
-   *
-   * Code goes here
-   *
-   */
+  const data = {
+    user,
+    text,
+  };
+
+  socket.emit("msg:post", data);
 }
 
 function render() {
